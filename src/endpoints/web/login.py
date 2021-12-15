41ef5650b1a65985864c96767633132982579aad
jarco <jarco@elnino.tech>
2021-12-15 22:04:37 +0100

    Implement basic user registration & login flow

--- src/endpoints/web/login.py
+++ src/endpoints/web/login.py
@@ -1,7 +1,42 @@
+from passlib.hash import sha256_crypt
 from quart import Blueprint, request, jsonify
+from dataclasses import dataclass
+from quart_schema import validate_request
+from playhouse.shortcuts import model_to_dict
+from quart_jwt_extended import create_access_token
+
+from src.database.users import Users
+from src.database.database import database as db
 
 login_endpoint = Blueprint('login_endpoint', __name__)
 
+
+@dataclass
+class LoginRequest:
+    email: str
+    password: str
+
+
 @login_endpoint.route('/', methods=['POST'])
-async def login():
-    return jsonify({'message': 'Hello World!'})
+@validate_request(LoginRequest)
+async def login(data: LoginRequest):
+    req = await request.get_json()
+    email = req.get('email')
+    plain_password = req.get('password')
+    with db.atomic():
+        user = Users.get_or_none(Users.email == email)
+    if not user:
+        return jsonify({'message': 'User not found', "success": False}), 404
+    verified = verify_password(user, plain_password)
+    if not user or not verified:
+        return jsonify({'message': 'Password or email incorrect.', 'success': False}), 403
+    user_dict = model_to_dict(user)
+    user_dict.pop('password')
+    user_dict.pop('password_reset_token')
+    user_dict['company'].pop("api_key")
+    token = f"Bearer {create_access_token(identity=email)}"
+    return jsonify({'message': user_dict, "token": token, 'success': True})
+
+
+def verify_password(user: Users, plain_pass: str) -> bool:
+    return sha256_crypt.verify(plain_pass, user.password)
--- src/endpoints/web/register.py
+++ src/endpoints/web/register.py
@@ -1,9 +1,14 @@
 from quart import Blueprint, request, jsonify
 from dataclasses import dataclass
 from quart_schema import validate_request
+from playhouse.shortcuts import model_to_dict
+from peewee import IntegrityError
+from passlib.hash import sha256_crypt
+
 
 from src.database.users import Users
-from src.database.company_details import CompanyDetails
+from src.database.company import Company
+from src.database.database import database as db
 
 register_endpoint = Blueprint('register_endpoint', __name__)
 
@@ -21,18 +26,29 @@ class RegisterRequest:
 async def register(data: RegisterRequest):
     req = await request.get_json()
     username = req.get('username')
-    password = req.get('password')
+    password_plain = req.get('password')
+    password = hash_password(password_plain)
     email = req.get('email')
     company = req.get('company')
-    company_det = CompanyDetails.get_or_none(company)
+    with db.atomic():
+        company_det = Company.get_or_none(company)
     if not company_det:
         return jsonify({'message': 'Company not found'}), 404
-    user = Users.create(username=username, password=password,
-                        email=email, company=company_det)
-    return jsonify({'message': user})
+
+    with db.atomic():
+        user = Users.create(username=username, password=password,
+                            email=email, company=company_det)
+    user_dict = model_to_dict(user)
+    user_dict.pop('password')
+    user_dict.pop('password_reset_token')
+    user_dict['company'].pop("api_key")
+    return jsonify({'message': user_dict})
 
 
 @register_endpoint.route('/password_forgot', methods=['POST'])
 async def password_forgot():
+    return jsonify({'message': 'This method is not implemented yet'})
 
-    return jsonify({'message': 'Hello World'})
+
+def hash_password(password: str) -> str:
+    return sha256_crypt.hash(password)
