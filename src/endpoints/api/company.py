91297a409086e38e5b26eb5a6f3dc4aa0b2deb29
jarco <jarco@elnino.tech>
2021-12-19 23:27:24 +0100

    Use generic helpers, change naming to be more precise, start implementing trips

--- src/endpoints/api/company.py
+++ src/endpoints/api/company.py
@@ -4,31 +4,22 @@ from quart import jsonify, request
 from quart.blueprints import Blueprint
 import quart_schema as qs
 from dataclasses import dataclass
+
+from config import CONFIG
 from ...database.company import Company
 from ...database.database import database
 
 from peewee import IntegrityError, InternalError
-# TODO move to helpers
-from pydantic.dataclasses import dataclass as pydantic_dataclass, is_builtin_dataclass
-from pydantic import ValidationError
+
+
+from typing import Dict, Optional, List, Tuple
 from pydantic.types import Json
 
-from werkzeug.exceptions import BadRequest
+from ..helpers.auth import hash_api_key, verify_api_key, get_random_string
+from ..helpers.validation import validate_request
 
-from typing import Dict, Optional, Union, List
-
-from passlib.hash import sha256_crypt
-
-import random
-import string
 
 company_endpoint = Blueprint('company', __name__)
-
-
-class RequestSchemaValidationError(BadRequest):
-    def __init__(self, validation_error: Union[TypeError, ValidationError]) -> None:
-        super().__init__()
-        self.validation_error = validation_error
 
 
 class CompanyTypes(Enum):
@@ -48,28 +39,11 @@ class CompanyRegisterSchema:
 
 
 @company_endpoint.route('/', methods=['GET', 'POST'])
-async def get_companies():
+async def companies():
     if request.method == "POST":
-        validate_request(model=CompanyRegisterSchema, source=(await request.get_json()))
-        print(request)
         req = await request.get_json()
-        api_key_plain = get_random_string()
-        hashed_api_key = hash_api_key(api_key_plain)
-        with database.atomic():
-            try:
-                company = Company.create(
-                    company_name=req["company_name"],
-                    company_location=req["company_location"],
-                    company_type=req["company_type"],
-                    company_api_url=req["company_api_url"],
-                    api_key=hashed_api_key,
-                )
-            except (IntegrityError, InternalError) as error:
-                return jsonify({"error": "Duplicate entry."}), 422
-        company_dict = model_to_dict(company)
-        # Return the plain text api key upon registration
-        company_dict["api_key"] = api_key_plain
-        return jsonify(company_dict), 201
+        validate_request(model=CompanyRegisterSchema, source=req)
+        return create_company(req)
     elif request.method == "GET":
         return jsonify(get_companies()), 200
 
@@ -81,29 +55,6 @@ async def get_company(company_id: int):
         return jsonify({'message': 'Company not found'}), 404
 
     return jsonify(company_dict), 200
-
-
-def validate_request(model, source: Json) -> bool:
-    model_class = pydantic_dataclass(model)
-    try:
-        model = model_class(**source)
-    except (TypeError, ValidationError) as error:
-        raise RequestSchemaValidationError(error)
-    return True
-
-
-def get_random_string() -> str:
-    chars = string.ascii_letters + string.punctuation
-    size = 12
-    return ''.join(random.choice(chars) for _ in range(size))
-
-
-def hash_api_key(api_key: str) -> str:
-    return sha256_crypt.hash(api_key)
-
-
-def verify_api_key(company: Company, api_key: str) -> bool:
-    return sha256_crypt.verify(api_key, company.api_key)
 
 
 def get_company_by_id(company_id: int) -> Optional[Dict]:
@@ -123,3 +74,26 @@ def get_companies() -> List[Dict]:
         company_dict.pop("api_key")
         companies.append(company_dict)
     return companies
+
+
+def create_company(req: Json) -> Tuple:
+    api_key_plain = get_random_string()
+    hashed_api_key = hash_api_key(api_key_plain)
+    with database.atomic():
+        try:
+            company = Company.create(
+                company_name=req["company_name"],
+                company_location=req["company_location"],
+                company_type=req["company_type"],
+                company_api_url=req["company_api_url"],
+                api_key=hashed_api_key,
+            )
+        except (IntegrityError, InternalError):
+            return jsonify({"error": "Duplicate entry."}), 422
+    company_dict = model_to_dict(company)
+    # Return the plain text api key upon registration, unless api keys have not been enabled.
+    if CONFIG.auth['api_key_enabled']:
+        company_dict["api_key"] = api_key_plain
+    else:
+        company_dict.pop("api_key")
+    return jsonify(company_dict), 201
--- src/endpoints/api/trips.py
+++ src/endpoints/api/trips.py
@@ -1,8 +1,60 @@
+from uuid import uuid4, UUID
+from enum import Enum
+from typing import List, Dict, Tuple
+from dataclasses import dataclass
+from pydantic.types import Json
 from quart import Blueprint, request, jsonify
+
+from endpoints.helpers.validation import validate_request
 
 trips_endpoints = Blueprint('trips_endpoints', __name__)
 
 
-@trips_endpoints.route('/', methods=['GET'])
-async def get_trip():
-    return jsonify({'trip': []}), 200
+class CargoTypes(Enum):
+    """
+    Enum for cargo types
+    """
+    ALCOHOL = 'Alcohol'
+    BEER = 'Beer'
+    WINE = 'Wine'
+    TOBACCO = 'Tobacco'
+    PETROL = 'Petrol'
+    DIESEL = 'Diesel'
+    OIL = 'Oil'
+    LPG = 'LPG'
+    JUICE = 'Juice'
+    VEGETABLE_JUICE = 'Vegetable Juice'
+    FRUIT_JUICE = 'Fruit Juice'
+    MINERAL_WATER = 'Mineral Water'
+    UNTAXED = 'Untaxed'
+
+
+@dataclass
+class TripRequestSchema:
+    id: UUID
+    start_location: str
+    pickup_location: str
+    destination_location: str
+    payload: List[Dict[CargoTypes, int]]
+    truck_license_plate: str
+    truck_location: str
+    current_truck_load: int
+
+
+@trips_endpoints.route('/', methods=['GET', 'POST'])
+async def trips():
+    if request.method == "POST":
+        req = await request.get_json()
+        print(req)
+        validate_request(model=TripRequestSchema, source=req)
+        return create_trip(req)
+    else:
+        return get_trips()
+
+
+def create_trip(req: Json) -> Tuple[Json, int]:
+    return jsonify("test"), 201
+
+
+def get_trips() -> Tuple[Json, int]:
+    return jsonify("test"), 200
