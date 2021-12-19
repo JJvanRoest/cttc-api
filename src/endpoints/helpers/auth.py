d6746332861835c18fc9374505919cac80949c48
jarco <jarco@elnino.tech>
2021-12-19 23:26:52 +0100

    Extract functions to generic helpers

---
+++ src/endpoints/helpers/auth.py
@@ -0,0 +1,19 @@
+from passlib.hash import sha256_crypt
+import random
+import string
+
+from ...database.company import Company
+
+
+def get_random_string() -> str:
+    chars = string.ascii_letters + string.punctuation
+    size = 12
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def hash_api_key(api_key: str) -> str:
+    return sha256_crypt.hash(api_key)
+
+
+def verify_api_key(company: Company, api_key: str) -> bool:
+    return sha256_crypt.verify(api_key, company.api_key)
---
+++ src/endpoints/helpers/validation.py
@@ -0,0 +1,22 @@
+from pydantic.dataclasses import dataclass as pydantic_dataclass, is_builtin_dataclass
+from pydantic import ValidationError
+from pydantic.types import Json
+from werkzeug.exceptions import BadRequest
+
+
+from typing import Union
+
+
+class RequestSchemaValidationError(BadRequest):
+    def __init__(self, validation_error: Union[TypeError, ValidationError]) -> None:
+        super().__init__()
+        self.validation_error = validation_error
+
+
+def validate_request(model, source: Json) -> bool:
+    model_class = pydantic_dataclass(model)
+    try:
+        model = model_class(**source)
+    except (TypeError, ValidationError) as error:
+        raise RequestSchemaValidationError(error)
+    return True
